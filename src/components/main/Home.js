8a098a6db99d8302dd283bab573c633ee6607742
olegs7771 <olegs7771@gmail.com>
2020-11-07 09:44:18 +0200

    ok

--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -1,12 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import logo from "../../img/logo.png";
+import sprite from "../../img/sprite.svg";
 
- class Home extends Component {
+class Home extends Component {
   render() {
     return (
-      <div >
-        Home
+      <div className="container">
+        <header className="header">
+          <img src={logo} alt="logo" class="logo" />
+          <form action="#" className="search">
+            <input
+              type="text"
+              className="search__input"
+              placeholder="Search Hotels"
+            />
+            <div className="button search__button">
+              <svg className="search__icon">
+                <use href={sprite + "#icon-magnifying-glass"} />
+              </svg>
+            </div>
+          </form>
+        </header>
+        <div className="content">
+          <nav className="sidebar">Navigation</nav>
+          <main className="hotel-view">Hotel View</main>
+        </div>
       </div>
-    )
+    );
   }
 }
 export default Home;
