94e4e4ac608b90bc21d452cb0e5d618753b82b65
olegs7771 <olegs7771@gmail.com>
2020-11-07 18:11:09 +0200

    ok

--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -1,25 +1,51 @@
 import React, { Component } from "react";
 import logo from "../../img/logo.png";
 import sprite from "../../img/sprite.svg";
+import user_photo from "../../img/user.jpg";
+import { FaSearch } from "react-icons/fa";
 
 class Home extends Component {
   render() {
     return (
       <div className="container">
         <header className="header">
-          <img src={logo} alt="logo" class="logo" />
+          <img src={logo} alt="logo" className="logo" />
           <form action="#" className="search">
             <input
               type="text"
               className="search__input"
               placeholder="Search Hotels"
             />
-            <div className="button search__button">
+            <button className="button search__button">
               <svg className="search__icon">
                 <use href={sprite + "#icon-magnifying-glass"} />
               </svg>
-            </div>
+
+              {/* <FaSearch className="search__icon" /> */}
+            </button>
           </form>
+          <nav className="user-nav">
+            <div className="user-nav__icon-box">
+              <svg className="user-nav__icon">
+                <use href={sprite + "#icon-bookmark"} />
+              </svg>
+              <span className="user-nav__notification">7</span>
+            </div>
+            <div className="user-nav__icon-box">
+              <svg className="user-nav__icon">
+                <use href={sprite + "#icon-chat"} />
+              </svg>
+              <span className="user-nav__notification">7</span>
+            </div>
+            <div className="user-nav__user">
+              <img
+                src={user_photo}
+                alt="user_photo"
+                className="user-nav__user-photo"
+              />
+              <span className="user-nav__user-name">Oleg</span>
+            </div>
+          </nav>
         </header>
         <div className="content">
           <nav className="sidebar">Navigation</nav>
