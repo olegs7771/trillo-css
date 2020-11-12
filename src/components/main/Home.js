f98781841b37dc5e230e63559341f80047c4fb01
olegs7771 <olegs7771@gmail.com>
2020-11-12 21:24:58 +0200

    ok

--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -5,10 +5,10 @@ import user_photo from "../../img/user.jpg";
 import hotel1_img from "../../img/hotel-1.jpg";
 import hotel2_img from "../../img/hotel-2.jpg";
 import hotel3_img from "../../img/hotel-3.jpg";
-import user_1_img from '../../img/user-1.jpg'
-import user_2_img from '../../img/user-2.jpg'
-import user_3_img from '../../img/user-3.jpg'
-import user_4_img from '../../img/user-4.jpg'
+import user_1_img from "../../img/user-1.jpg";
+import user_2_img from "../../img/user-2.jpg";
+import user_3_img from "../../img/user-3.jpg";
+import user_4_img from "../../img/user-4.jpg";
 
 class Home extends Component {
   render() {
@@ -153,93 +153,114 @@ class Home extends Component {
                 <div className="overview__rating-count">429 votes</div>
               </div>
             </div>
-            {/* DETAIL   contain 2 columns description + user reviews*/} 
+            {/* DETAIL   contain 2 columns description + user reviews*/}
             <div className="detail">
               <div className="description">
-                    <p className="paragraph">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium unde fuga, quae vel quibusdam iusto! Iusto nemo odit sint assumenda repellendus voluptas maxime repellat qui debitis. Ut cum dolores voluptas.
-                    </p>
-                    <p className="paragraph">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium unde fuga, quae vel quibusdam iusto! .
-                    </p>
-                      <ul className="list">
-                        <li className="list__item">Close to the beach</li>
-                        <li className="list__item">Breakfast Included</li>
-                        <li className="list__item">Free airport shuttle</li>
-                        <li className="list__item">Free wifi in all rooms</li>
-                        <li className="list__item">Pets allowed</li>
-                        <li className="list__item">We speak all languages</li>
-                        <li className="list__item">A/C in all rooms</li>
-                        <li className="list__item">Perfect for all families</li>
-                      </ul>
-                      <div className="recommend">
-                        <p className="recommend__count">
-                          Lucy and 3 more friends recommended this
-                        </p>
-                        <div className="recommend__friends">
-                          <img src={user_1_img} alt="user 1" className="recommend__photo"/>
-                          <img src={user_2_img} alt="user 2" className="recommend__photo"/>
-                          <img src={user_3_img} alt="user 3" className="recommend__photo"/>
-                          <img src={user_4_img} alt="user 4" className="recommend__photo"/>
-                        </div>
-                      </div>
+                <p className="paragraph">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Praesentium unde fuga, quae vel quibusdam iusto! Iusto nemo
+                  odit sint assumenda repellendus voluptas maxime repellat qui
+                  debitis. Ut cum dolores voluptas.
+                </p>
+                <p className="paragraph">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Praesentium unde fuga, quae vel quibusdam iusto! .
+                </p>
+                <ul className="list">
+                  <li className="list__item">Close to the beach</li>
+                  <li className="list__item">Breakfast Included</li>
+                  <li className="list__item">Free airport shuttle</li>
+                  <li className="list__item">Free wifi in all rooms</li>
+                  <li className="list__item">Pets allowed</li>
+                  <li className="list__item">We speak all languages</li>
+                  <li className="list__item">A/C in all rooms</li>
+                  <li className="list__item">Perfect for all families</li>
+                </ul>
+                <div className="recommend">
+                  <p className="recommend__count">
+                    Lucy and 3 more friends recommended this
+                  </p>
+                  <div className="recommend__friends">
+                    <img
+                      src={user_1_img}
+                      alt="user 1"
+                      className="recommend__photo"
+                    />
+                    <img
+                      src={user_2_img}
+                      alt="user 2"
+                      className="recommend__photo"
+                    />
+                    <img
+                      src={user_3_img}
+                      alt="user 3"
+                      className="recommend__photo"
+                    />
+                    <img
+                      src={user_4_img}
+                      alt="user 4"
+                      className="recommend__photo"
+                    />
+                  </div>
+                </div>
               </div>
-                      <div className="user-reviews">
-        <figure className="review">
-            <blockquote className='review__text'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                 Magni officia perspiciatis pariatur cumque quod dolorem fugiat quae ducimus odio ipsa. 
-                Quidem necessitatibus, voluptatem cumque culpa totam eaque at iure harum!
-            </blockquote>
-         <figcaption className="review__user">
-            <img src={user_1_img} alt="user 1" className="review__photo"/>
-            <div className="review__user-box">
-              <p className="review__user-name">
-                John Smith
-              </p>
-              <p className="review__user-date">
-                Feb 23rd,2020
-              </p>
-            </div>
-            <div className="review__rating">
-              7.8
-            </div>
-        </figcaption>
-        </figure>
-        <figure className="review">
-            <blockquote className='review__text'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                 Magni officia perspiciatis pariatur cumque quod dolorem fugiat quae ducimus odio ipsa. 
-                Quidem necessitatibus, voluptatem cumque culpa totam eaque at iure harum!
-            </blockquote>
-         <figcaption className="review__user">
-            <img src={user_2_img} alt="user 1" className="review__photo"/>
-            <div className="review__user-box">
-              <p className="review__user-name">
-                Karen Brown
-              </p>
-              <p className="review__user-date">
-                Feb 22rd,2020
-              </p>
-            </div>
-            <div className="review__rating">
-              5.2
-            </div>
-        </figcaption>
-        </figure>
+              <div className="user-reviews">
+                <figure className="review">
+                  <blockquote className="review__text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Magni officia perspiciatis pariatur cumque quod dolorem
+                    fugiat quae ducimus odio ipsa. Quidem necessitatibus,
+                    voluptatem cumque culpa totam eaque at iure harum!
+                  </blockquote>
+                  <figcaption className="review__user">
+                    <img
+                      src={user_1_img}
+                      alt="user 1"
+                      className="review__photo"
+                    />
+                    <div className="review__user-box">
+                      <p className="review__user-name">John Smith</p>
+                      <p className="review__user-date">Feb 23rd,2020</p>
+                    </div>
+                    <div className="review__rating">7.8</div>
+                  </figcaption>
+                </figure>
+                <figure className="review">
+                  <blockquote className="review__text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Magni officia perspiciatis pariatur cumque quod dolorem
+                    fugiat quae ducimus odio ipsa. Quidem necessitatibus,
+                    voluptatem cumque culpa totam eaque at iure harum!
+                  </blockquote>
+                  <figcaption className="review__user">
+                    <img
+                      src={user_2_img}
+                      alt="user 1"
+                      className="review__photo"
+                    />
+                    <div className="review__user-box">
+                      <p className="review__user-name">Karen Brown</p>
+                      <p className="review__user-date">Feb 22rd,2020</p>
+                    </div>
+                    <div className="review__rating">5.2</div>
+                  </figcaption>
+                </figure>
 
-        <button className="btn-inline">Show All <span>&rarr;</span></button>
-            </div>
-
+                <button className="btn-inline">
+                  Show All <span>&rarr;</span>
+                </button>
+              </div>
             </div>
             {/* DETAIL END  */}
-<div className="cta">
-  <h2 className="cta__book-now">
-    Good news! we have 4 free rooms  for your selected dates!
-  </h2>
-  <button className="btn">Only 4 rooms left</button>
-</div>
-
+            <div className="cta">
+              <h2 className="cta__book-now">
+                Good news! we have 4 free rooms for your selected dates!
+              </h2>
+              <button className="btn">
+                <span className="btn__visible">Book now</span>
+                <span className="btn__invisible">Only 4 rooms left</span>
+              </button>
+            </div>
           </main>
         </div>
       </div>
